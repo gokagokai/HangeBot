d289d600303de202da2f49bac89391ae21badb31
GokaGokai <gokagokaigokai@gmail.com>
2024-06-13 21:18:22 -0400

    Add `/template` for prompt of characters

--- cogs/sd.py
+++ cogs/sd.py
@@ -5,15 +5,19 @@ from discord import app_commands
 from discord.ext import commands
 from discord.ext.commands import Context
 
+import toml
 from modules import LoadDistributionManager
 from api import AutoWebUi
+
+with open(f"config.toml") as file:
+    config = toml.load(file)
 
 class Sd(commands.Cog, name="sd"):
     def __init__(self, bot) -> None:
         self.bot = bot
-        ips = self.bot.config['webui_ips'].items()
+        ips = config['webui_ips'].items()
         ips = [ip[1] for ip in ips]
-        self.load_distributor = LoadDistributionManager.LoadDist(ips, self.bot.config)
+        self.load_distributor = LoadDistributionManager.LoadDist(ips, config)
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -72,57 +76,67 @@ class Sd(commands.Cog, name="sd"):
         guidance_scale="CFG scale",
         sampler="Sampling method"
     )
+    @app_commands.choices(
+        aspect_ratio=[
+            app_commands.Choice(name=key, value=key) 
+            for key in config['aspect_ratios'].keys()
+        ],
+        sampler=[
+            app_commands.Choice(name=sampler, value=sampler) 
+            for sampler in config['command_params']['samplers']
+        ]
+    )
     async def generate(
         self, ctx: Context, 
         prompt: str,
         negative_prompt: str = None,
         aspect_ratio: str = None,
-        steps: int = None,
+        steps: app_commands.Range[int, config['command_params']['min_steps'], config['command_params']['max_steps']] = None,
         seed: int = None,
         guidance_scale: float = None,
         sampler: str = None
     ):
-        negative_prompt = negative_prompt or self.bot.config['command_params']['default_negative']
-        aspect_ratio = aspect_ratio or self.bot.config['command_params']['default_ratio']
-        steps = steps or self.bot.config['command_params']['default_steps']
+        negative_prompt = negative_prompt or config['command_params']['default_negative']
+        aspect_ratio = aspect_ratio or config['command_params']['default_ratio']
+        steps = steps or config['command_params']['default_steps']
         seed = seed or -1
-        guidance_scale = guidance_scale or self.bot.config['command_params']['default_cfg']
-        sampler = sampler or self.bot.config['command_params']['default_sampler']
+        guidance_scale = guidance_scale or config['command_params']['default_cfg']
+        sampler = sampler or config['command_params']['default_sampler']
 
         # Check for banned words
-        search = prompt if self.bot.config['blacklist']['allow_in_negative'] else ' '.join([prompt, negative_prompt])
-        for word in self.bot.config['blacklist']['words']:
+        search = prompt if config['blacklist']['allow_in_negative'] else ' '.join([prompt, negative_prompt])
+        for word in config['blacklist']['words']:
             for word2 in search.translate(str.maketrans('', '', string.punctuation)).split():
                 if word.lower() == word2.lower():
                     print(f'Denied -- {ctx.author.name} tried to use banned word: {word}')
-                    await ctx.send(f'You tried to use a banned word.\n `Word: {word}`', ephemeral=True)
+                    await ctx.send(f'**You tried to use a banned word.**\n `Word: {word}`', ephemeral=True)
                     return
 
         # Prevent randomness from going wild when short prompt
         if len(prompt) <= 20:
             if negative_prompt:
-                negative_prompt += ', ' + ', '.join(self.bot.config['blacklist']['words'])
+                negative_prompt += ', ' + ', '.join(config['blacklist']['words'])
             else:
-                negative_prompt = ', '.join(self.bot.config['blacklist']['words'])
+                negative_prompt = ', '.join(config['blacklist']['words'])
 
         # Check for banned nsfw words
-        if str(ctx.channel.id) not in self.bot.config['blacklist']['nsfw_channels']:
-            for word in self.bot.config['blacklist']['nsfw_words']:
+        if str(ctx.channel.id) not in config['blacklist']['nsfw_channels']:
+            for word in config['blacklist']['nsfw_words']:
                 for word2 in search.translate(str.maketrans('', '', string.punctuation)).split():
                     if word.lower() == word2.lower():
                         print(f'Denied -- {ctx.author.name} tried to use banned nsfw word: {word}')
-                        await ctx.send(f'Chill bro keep it Sfw.\n `Word: {word}`', ephemeral=True)
+                        await ctx.send(f'**Chill bro keep it Sfw.**\n `Word: {word}`', ephemeral=True)
                         return
             
             if len(prompt) <= 20:
                 if negative_prompt:
-                    negative_prompt += ', ' + ', '.join(self.bot.config['blacklist']['nsfw_words'])
+                    negative_prompt += ', ' + ', '.join(config['blacklist']['nsfw_words'])
                 else:
-                    negative_prompt = ', '.join(self.bot.config['blacklist']['nsfw_words'])
+                    negative_prompt = ', '.join(config['blacklist']['nsfw_words'])
 
         # Process request
         print(f'Request -- {ctx.author.name} -- Prompt: {prompt}')
-        width, height = self.bot.config['aspect_ratios'][aspect_ratio]
+        width, height = config['aspect_ratios'][aspect_ratio]
         queue_obj = AutoWebUi.QueueObj(
             event_loop=asyncio.get_event_loop(),
             ctx=ctx,
@@ -143,16 +157,26 @@ class Sd(commands.Cog, name="sd"):
         if response == LoadDistributionManager.Status.QUEUED:
             await ctx.send(f'`Queue Position: {info}`')
         elif response == LoadDistributionManager.Status.IN_QUEUE:
-            embed = discord.Embed(
-                                  description=f'**Chill bro I only have 1 GPU**.\n'
-                                              f'`Your Position: {info + 1}`',
-                                  color=0xFFD700)
-            await ctx.send(embed=embed, ephemeral=True)
+            await ctx.send(f'**Chill bro I only have 1 GPU.**\n `Your Position: {info + 1}`', ephemeral=True)
         else:
             embed = discord.Embed(title='Encountered an error: ',
                                   description=str(response),
                                   color=0xff0000)
             await ctx.send(embed=embed)
+
+    @commands.hybrid_command(
+        name="template",
+        description="Get a prompt template of a character"
+    )
+    @app_commands.describe(character="Character template")
+    @app_commands.choices(character=[
+        app_commands.Choice(name=key, value=key) 
+        for key in config['template'].keys()
+    ])
+    async def template(self, ctx: Context, character: str):
+        template_value = config['template'][character]
+        await ctx.send(f"**Copy paste the prompt of `{character}` below (you can also adjust to your likings):**")
+        await ctx.channel.send(template_value)
 
 async def setup(bot) -> None:
     await bot.add_cog(Sd(bot))
